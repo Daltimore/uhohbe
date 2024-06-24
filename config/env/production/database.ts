1c5fd75cae03cbd53ff5f3f4dbe30c2bd73b912f
Daltimore <obinnaleo78@gmail.com>
2024-06-25 00:56:40 +0100

    Configure Strapi for Heroku deployment

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,19 +1,14 @@
-// path: ./config/env/production/database.ts
-
-import { parse } from 'pg-connection-string';
-const config = parse(process.env.DATABASE_URL);
-
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
       ssl: {
-        rejectUnauthorized: false
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
       },
     },
     debug: false,
--- config/server.ts
+++ config/server.ts
@@ -1,10 +1,8 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', process.env.PORT || 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
 });
+
